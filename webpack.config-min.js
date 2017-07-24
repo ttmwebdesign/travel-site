d764b89b787146dad8d88d69cbb2f2d324922169
Niki <nikiwolf@gmail.com>
2017-07-24 16:34:54 -0700

    Completed lazyloading of images.

--- webpack.config-min.js
+++ webpack.config-min.js
@@ -1,1 +1,25 @@
-const path=require("path");module.exports={entry:"./app/assets/scripts/app.js",output:{path:path.join(__dirname,"./app/temp/scripts"),filename:"app.js"},module:{loaders:[{loader:"babel-loader",query:{presets:["es2015"]},test:/\.js$/,exclude:/node_modules/}]}};
+const path = require('path');
+
+module.exports = {
+	entry: {
+		App: "./app/assets/scripts/app.js",
+		Vendor: "./app/assets/scripts/Vendor.js"
+	},
+	output: {
+		path: path.join(__dirname, "./app/temp/scripts"),
+		filename: "[name].js"
+	},
+	module: {
+		loaders: [
+			{
+				loader: 'babel-loader',
+				query: {
+					presets: ['es2015']
+				},
+				test: /\.js$/,
+				exclude: /node_modules/
+			}
+		]
+	}
+}
+
