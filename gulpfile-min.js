ae2b79ed94d634a8f9343a70dad9187607a85a9a
Niki <nikiwolf@gmail.com>
2017-07-25 10:41:08 -0700

    Completed gulp build task.

--- gulpfile-min.js
+++ gulpfile-min.js
@@ -1,9 +1,1 @@
-require('./gulp/tasks/styles');
-require('./gulp/tasks/watch');
-require('./gulp/tasks/sprites');
-require('./gulp/tasks/scripts');
-require('./gulp/tasks/modernizr');
-
-
-
-
+require("./gulp/tasks/styles"),require("./gulp/tasks/watch"),require("./gulp/tasks/sprites"),require("./gulp/tasks/scripts"),require("./gulp/tasks/modernizr"),require("./gulp/tasks/build");
